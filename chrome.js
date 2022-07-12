9e9922bce2defa98988c5c74ab5ba480786bfd9d
@fntysm <zainebb7k9h1@gmail.com>
2022-07-12 20:08:14 +0100

    worked

--- chrome.js
+++ chrome.js
@@ -1,9 +1,14 @@
-var save = document.getElementById("input-btn");
-save.addEventListener("click",function(){
-    save.innerHTML = "INPUT SAVED";
-    save.style.border = "15px solid navy";
-})
+const save = document.getElementById("input-btn");
 save.addEventListener("mouseout",function(){
     save.innerHTML = "SAVE INPUT"
     save.style.border = "";
+})
+
+let myLinks = [];
+const inputEl = document.getElementById("input-elt");
+save.addEventListener("click",function(){
+    save.innerHTML = "INPUT SAVED";
+    save.style.border = "5px solid navy";
+    myLinks.push(inputEl.value);
+    console.log(myLinks);
 })
