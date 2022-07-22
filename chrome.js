5c71123e5aec249c484fa1c37a4bd1cc11276dd2
unknown <zainebb7k9h1@gmail.com>
2022-07-22 23:53:07 +0100

    some edits to better use

--- chrome.js
+++ chrome.js
@@ -1,9 +1,14 @@
+// title of the project
 const save = document.getElementById("input-btn");
+// delete button
 const deleteBtn = document.getElementById("delete-btn");
+// save the tqb button
 const tabBtn = document.getElementById("tab-btn");
-let myLinks = []; 
+// an array to store the input text data (save)
+let myLinks = [];
+// to push the items of the array inside the local storage 
 const alink = JSON.parse(localStorage.getItem("myLinks"));
-
+// mouseout of the save button
 save.addEventListener("mouseout",function(){
     save.innerHTML = "SAVE INPUT"
 })
@@ -14,8 +19,19 @@ save.addEventListener("click",function(){
     myLinks.push(inputEl.value);
     inputEl.value = "";
     localStorage.setItem("myLinks", JSON.stringify(myLinks));
-    renderLinks(myLinks);
+    renderInputText(myLinks);
 })
+const renderInputText = (myLinks) => {
+    let listItems = "";
+    let ulEl = document.getElementById("ulEl");
+    for(let i=0;i<myLinks.length;i++){
+    listItems += `
+    <li>
+    <h3 target='_blank'> ${myLinks[i]} </h3>
+    </li>`;
+     }
+    ulEl.innerHTML = listItems;
+}
 function renderLinks(myLinks){
     let listItems = "";
     let ulEl = document.getElementById("ulEl");
